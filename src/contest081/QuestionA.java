5bc891e889fca291f833a69b5b4139bd822711bd
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-04-19 18:06:36 +0900

    ABCBeginnersSelection2問目完了

--- src/contest081/QuestionA.java
+++ src/contest081/QuestionA.java
@@ -1,10 +1,26 @@
 package contest081;
 
+import java.util.Scanner;
+
 public class QuestionA {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		Scanner sc = new Scanner(System.in);
 
+		String str = sc.next();
+		String ss[] = new String[3];
+		sc.close();
+		
+		int count = 0;
+		for(int i = 0; i < ss.length; i++) {
+			ss[i] = str.substring(i , i + 1);
+			if(ss[i].equals("1")) {
+				count++;
+			}else {
+				continue;
+			}
+		}
+		
+		System.out.println(count);
 	}
-
 }
