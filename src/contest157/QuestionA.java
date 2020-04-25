ccb96cf98e20907b016de9ee9ce2937e86823afc
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-04-26 03:39:53 +0900

    ABC157 A問題AC

---
+++ src/contest157/QuestionA.java
@@ -0,0 +1,18 @@
+package contest157;
+
+import java.util.Scanner;
+
+public class QuestionA {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = Integer.parseInt(sc.next());
+		sc.close();
+		
+		if( N % 2 == 0) {
+			System.out.println(N / 2);
+		}else {
+			System.out.println(N / 2 + 1);
+		}
+	}
+}
