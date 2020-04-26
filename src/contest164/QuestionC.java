01a6a983a9867db33a8eea32128ae153e7d0dcbd
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-04-26 19:03:37 +0900

    準備完了

---
+++ src/contest164/QuestionC.java
@@ -0,0 +1,10 @@
+package contest164;
+
+public class QuestionC {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ src/contest164/QuestionD.java
@@ -0,0 +1,9 @@
+package contest164;
+
+public class QuestionD {
+
+	public static void main(String[] args) {
+
+	}
+
+}
