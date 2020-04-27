f3f75a96110eb160dc891c3c7e51e8776ffa4709
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-04-27 22:10:46 +0900

    ABC147 A問題AC

--- src/contest147/QuestionA.java
+++ src/contest147/QuestionA.java
@@ -1,23 +1,21 @@
-package contest148;
+package contest147;
 
 import java.util.Scanner;
 
 public class QuestionA {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int A = Integer.parseInt(sc.next());
 		int B = Integer.parseInt(sc.next());
+		int C = Integer.parseInt(sc.next());
 		sc.close();
 
-		//掛け算の値で条件分岐
-		int check = A * B;
-		if(check == 2) {
-			System.out.println(3);
-		}else if(check == 3) {
-			System.out.println(2);
+		//3つの数の値の和で条件分岐
+		int check = A + B + C;
+		if(check <= 21) {
+			System.out.println("win");
 		}else {
-			System.out.println(1);
+			System.out.println("bust");
 		}
 	}
 }
