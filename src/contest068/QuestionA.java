ca7ae3a6eb7d34ebeb5b0675736c090aedc8ad2b
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-04-29 17:17:24 +0900

    ABC068 A問題AC

---
+++ src/contest068/QuestionA.java
@@ -0,0 +1,14 @@
+package contest068;
+
+import java.util.Scanner;
+
+public class QuestionA {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = Integer.parseInt(sc.next());
+		sc.close();
+
+		System.out.println("ABC" + N);
+	}
+}
