7f91116ec210882134bb820290407ab52907356e
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-04-30 22:46:59 +0900

    ABC029 A問題AC

---
+++ src/contest029/QuestionA.java
@@ -0,0 +1,15 @@
+package contest029;
+
+import java.util.Scanner;
+
+public class QuestionA {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String W = sc.next();
+		sc.close();
+
+		System.out.println(W + "s");
+	}
+
+}
