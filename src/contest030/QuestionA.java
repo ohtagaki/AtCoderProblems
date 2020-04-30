b0b85e209518ea19981956b3afe533abaf131f66
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-04-30 22:23:10 +0900

    ABC030 A問題AC

--- src/contest030/QuestionA.java
+++ src/contest030/QuestionA.java
@@ -1,10 +1,28 @@
 package contest030;
 
+import java.util.Scanner;
+
 public class QuestionA {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		Scanner sc = new Scanner(System.in);
+		double A = Double.parseDouble(sc.next());
+		double B = Double.parseDouble(sc.next());
+		double C = Double.parseDouble(sc.next());
+		double D = Double.parseDouble(sc.next());
+		sc.close();
+
+		double tk = B / A;
+		double ao = D / C;
+
+		//改行に注意
+		if(tk == ao) {
+			System.out.println("DRAW");
+		}else if(tk < ao) {
+			System.out.println("AOKI");
+		}else {
+			System.out.println("TAKAHASHI");
+		}
 
 	}
-
 }
