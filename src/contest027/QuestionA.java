635d0c757a305f490a195ec1f853b59b779c9bef
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-05-01 22:10:07 +0900

    ABC027 A問題AC

---
+++ src/contest027/QuestionA.java
@@ -0,0 +1,27 @@
+package contest027;
+
+import java.util.Scanner;
+
+public class QuestionA {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = Integer.parseInt(sc.next());
+		int B = Integer.parseInt(sc.next());
+		int C = Integer.parseInt(sc.next());
+		sc.close();
+
+		if(A == B && A == C) {
+			System.out.println(A);
+		}else {
+			if(A == B) {
+				System.out.println(C);
+			}else if(B == C) {
+				System.out.println(A);
+			}else if(C == A) {
+				System.out.println(B);
+			}
+		}
+
+	}
+}
