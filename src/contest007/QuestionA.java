bc052dc36f2c95ecef8a5d703200be1ade2f7b54
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-05-01 23:52:55 +0900

    ABC007 A問題AC

---
+++ src/contest007/QuestionA.java
@@ -0,0 +1,15 @@
+package contest007;
+
+import java.util.Scanner;
+
+public class QuestionA {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.next());
+		sc.close();
+
+		System.out.println(n - 1);
+
+	}
+}
