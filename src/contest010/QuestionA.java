418c0ef9c4f6397572a78be4ef37711378a72083
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-05-01 20:09:47 +0900

    ABC010 A問題AC

---
+++ src/contest010/QuestionA.java
@@ -0,0 +1,15 @@
+package contest010;
+
+import java.util.Scanner;
+
+public class QuestionA {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String S = sc.next();
+		sc.close();
+
+		System.out.println(S + "pp");
+
+	}
+}
