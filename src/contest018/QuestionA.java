c0bdefa580cd15c53857603a6ec98f203f9d5898
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-05-01 21:04:54 +0900

    ABC018 A問題要復習

--- src/contest018/QuestionA.java
+++ src/contest018/QuestionA.java
@@ -7,13 +7,13 @@ public class QuestionA {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		sc.close();
 		final int n = 3;
 		int[] p1 = new int[n];
 		int[] p2 = new int[n];
 		for (int i = 0; i < n; i++) {
-			p1[i] = p2[i] = sc.nextInt();
+			p1[i] = p2[i] = Integer.parseInt(sc.next());
 		}
+		sc.close();
 		Arrays.sort(p2);
 
 		for (int i = 0; i < n; i++) {
